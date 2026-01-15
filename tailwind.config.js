0705c3efc5509027c9562e264e041a8ea64fe381
Godson Sunny <godsonsunny190>
2026-01-15 21:36:39 +0530

    html up

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '24px',
+        lg: '32px',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1520px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, #67c0ff 0%, #0075bf 100%)',
+      },
+    },
+  },
+};
